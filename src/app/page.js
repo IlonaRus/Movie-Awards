b61e7c8e1633cd6d02d80a1beac42bbcc99dff84
Ilona <ilonarus@hotmail.com>
2023-11-29 16:44:29 +0100

    Add card styling

--- src/app/page.js
+++ src/app/page.js
@@ -5,19 +5,111 @@ export default function Home() {
         <main className={styles.main}>
             <nav className={styles.navbar}>
                 <ul className={styles.list}>
-                    <li><a class="current" href="index.html" title="Ga naar de homepage">Home</a></li>
-                    <li><a href="xxx.html" title="Films">Films</a></li>
-                    <li><a href="xxx.html" title="Categorien">Categorien</a></li>
-                    <li><a href="xxx.html" title="Stemmen">Stemmen</a></li>
+                    <li>
+                        <a
+                            class="current"
+                            href="index.html"
+                            title="Ga naar de homepage"
+                        >
+                            Home
+                        </a>
+                    </li>
+                    <li>
+                        <a href="xxx.html" title="Films">
+                            Films
+                        </a>
+                    </li>
+                    <li>
+                        <a href="xxx.html" title="Categorien">
+                            Categorien
+                        </a>
+                    </li>
+                    <li>
+                        <a href="xxx.html" title="Stemmen">
+                            Stemmen
+                        </a>
+                    </li>
                 </ul>
             </nav>
             <div className={styles.center}>
-                <div className={styles.container}>
-                    <h1>Film awards</h1>
+                <div>
+                    <div className={styles.container}>
+                        <h1>Film awards</h1>
+                    </div>
+
                     <div className={styles.card}>
-                        <div className={styles['card-content']}>
-                            <h3>Films</h3>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde veritatis voluptatibus numquam quis officia a.</p>
+                        <div className={styles["card-content"]}>
+                            <div className={styles.column}>
+                                <div className={styles["column-1"]}>
+                                    <h2>Films</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Hic cumque quidem,
+                                        recusandae est libero laudantium.
+                                    </p>
+                                    <a
+                                        href="over.html"
+                                        className={styles.button}
+                                    >
+                                        Leer ons kennen
+                                    </a>
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles["column-2"]}>
+                                    <p>Plaatje</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles["card-content"]}>
+                            <div className={styles.column}>
+                                <div className={styles["column-1"]}>
+                                    <h2>Categorien</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Hic cumque quidem,
+                                        recusandae est libero laudantium.
+                                    </p>
+                                    <a
+                                        href="over.html"
+                                        className={styles.button}
+                                    >
+                                        Leer ons kennen
+                                    </a>
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles["column-2"]}>
+                                    <p>Plaatje</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.card}>
+                        <div className={styles["card-content"]}>
+                            <div className={styles.column}>
+                                <div className={styles["column-1"]}>
+                                    <h2>Stemmen</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet consectetur
+                                        adipisicing elit. Hic cumque quidem,
+                                        recusandae est libero laudantium.
+                                    </p>
+                                    <a
+                                        href="over.html"
+                                        className={styles.button}
+                                    >
+                                        Leer ons kennen
+                                    </a>
+                                </div>
+                            </div>
+                            <div className={styles.column}>
+                                <div className={styles["column-2"]}>
+                                    <p>Plaatje</p>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
